2b9e8088f83a19e6b7bb30f9f2a504a0e6d013e9
AExiaosong <aexiaosong@qq.com>
2019-12-16 00:47:35 +0800

    实现回复列表功能

--- src/main/java/com/xiaosong/community/controller/CommentController.java
+++ src/main/java/com/xiaosong/community/controller/CommentController.java
@@ -1,6 +1,6 @@
 package com.xiaosong.community.controller;
 
-import com.xiaosong.community.dto.CommentDTO;
+import com.xiaosong.community.dto.CommentCreateDTO;
 import com.xiaosong.community.dto.ResultDTO;
 import com.xiaosong.community.exception.CustomizeErrorCode;
 import com.xiaosong.community.model.Comment;
@@ -23,7 +23,7 @@ public class CommentController {
 
     @ResponseBody
     @RequestMapping(value = "/comment", method = RequestMethod.POST)
-    public Object post(@RequestBody CommentDTO commentDTO,
+    public Object post(@RequestBody CommentCreateDTO commentCreateDTO,
                        HttpServletRequest request) {
         User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
@@ -31,12 +31,12 @@ public class CommentController {
         }
 
         Comment comment = new Comment();
-        comment.setParentId(commentDTO.getParentId());
-        comment.setContent(commentDTO.getContent());
-        comment.setType(commentDTO.getType());
+        comment.setParentId(commentCreateDTO.getParentId());
+        comment.setContent(commentCreateDTO.getContent());
+        comment.setType(commentCreateDTO.getType());
         comment.setGmtCreate(System.currentTimeMillis());
         comment.setGmtModified(comment.getGmtCreate());
-        comment.setCommentator(1L);
+        comment.setCommentator(user.getId());
         comment.setLikeCount(0L);
         commentService.insert(comment);
         return ResultDTO.okOf();
--- src/main/java/com/xiaosong/community/controller/QuestionController.java
+++ src/main/java/com/xiaosong/community/controller/QuestionController.java
@@ -1,6 +1,8 @@
 package com.xiaosong.community.controller;
 
+import com.xiaosong.community.dto.CommentDTO;
 import com.xiaosong.community.dto.QuestionDTO;
+import com.xiaosong.community.service.CommentService;
 import com.xiaosong.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,19 +10,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 @Controller
 public class QuestionController {
 
     @Autowired
     private QuestionService questionService;
 
+    @Autowired
+    private CommentService commentService;
+
     @GetMapping("/question/{id}")
     public String question(@PathVariable("id") Long  id, Model model) {
         QuestionDTO questionDTO = questionService.getById(id);
+        List<CommentDTO> comments = commentService.listBuQuestionId(id);
         // 累加阅读数
         questionService.incView(id);
-        questionDTO.setViewCount(questionDTO.getViewCount() + 1);
         model.addAttribute("question", questionDTO);
+        model.addAttribute("comments", comments);
         return "question";
     }
 }
