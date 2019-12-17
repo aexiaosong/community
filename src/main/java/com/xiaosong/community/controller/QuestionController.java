040b4fb8fb5656db5b5d9ce1f9c0316214b1c435
AExiaosong <aexiaosong@qq.com>
2019-12-17 21:53:04 +0800

    提问页面相关问题展示

--- src/main/java/com/xiaosong/community/controller/QuestionController.java
+++ src/main/java/com/xiaosong/community/controller/QuestionController.java
@@ -26,10 +26,12 @@ public class QuestionController {
     public String question(@PathVariable("id") Long  id, Model model) {
         QuestionDTO questionDTO = questionService.getById(id);
         List<CommentDTO> comments = commentService.listByTargetId(id, CommentTypeEnum.QUESTION);
+        List<QuestionDTO> relatedQuestions = questionService.selectRelated(questionDTO);
         // 累加阅读数
         questionService.incView(id);
         model.addAttribute("question", questionDTO);
         model.addAttribute("comments", comments);
+        model.addAttribute("relatedQuestions", relatedQuestions);
         return "question";
     }
 }
--- src/main/java/com/xiaosong/community/mapper/QuestionExtMapper.java
+++ src/main/java/com/xiaosong/community/mapper/QuestionExtMapper.java
@@ -3,8 +3,11 @@ package com.xiaosong.community.mapper;
 import com.xiaosong.community.model.Question;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionExtMapper {
     int incView(Question record);
     int incCommentCount(Question record);
+    List<Question> selectRelated(Question question);
 }
