23864d288773085e5efd3c60c71554225f47212e
AExiaosong <aexiaosong@qq.com>
2019-12-23 21:08:27 +0800

    添加页脚

--- src/main/java/com/xiaosong/community/mapper/QuestionExtMapper.java
+++ src/main/java/com/xiaosong/community/mapper/QuestionExtMapper.java
@@ -1,5 +1,6 @@
 package com.xiaosong.community.mapper;
 
+import com.xiaosong.community.dto.QuestionQueryDTO;
 import com.xiaosong.community.model.Question;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ public interface QuestionExtMapper {
     int incView(Question record);
     int incCommentCount(Question record);
     List<Question> selectRelated(Question question);
+    Integer countBySearch(QuestionQueryDTO questionQueryDTO);
+    List<Question> selectBySearch(QuestionQueryDTO questionQueryDTO);
 }
--- src/main/java/com/xiaosong/community/service/QuestionService.java
+++ src/main/java/com/xiaosong/community/service/QuestionService.java
@@ -2,6 +2,7 @@ package com.xiaosong.community.service;
 
 import com.xiaosong.community.dto.PaginationDTO;
 import com.xiaosong.community.dto.QuestionDTO;
+import com.xiaosong.community.dto.QuestionQueryDTO;
 import com.xiaosong.community.exception.CustomizeErrorCode;
 import com.xiaosong.community.exception.CustomizeException;
 import com.xiaosong.community.mapper.QuestionExtMapper;
@@ -33,12 +34,19 @@ public class QuestionService {
     @Autowired
     private UserMapper userMapper;
 
-    public PaginationDTO list(Integer page, Integer size) {
+    public PaginationDTO list(String search, Integer page, Integer size) {
+        if (StringUtils.isNotBlank(search)) {
+            String[] tags = StringUtils.split(search, " ");
+            search = Arrays.stream(tags).collect(Collectors.joining("|"));
+        }
         PaginationDTO paginationDTO = new PaginationDTO();
 
         Integer totalPage;
 
-        Integer totalCount = (int) questionMapper.countByExample(new QuestionExample());
+        QuestionQueryDTO questionQueryDTO = new QuestionQueryDTO();
+        questionQueryDTO.setSearch(search);
+
+        Integer totalCount = questionExtMapper.countBySearch(questionQueryDTO);
 
         if (totalCount % size == 0) {
             totalPage = totalCount / size;
@@ -57,9 +65,9 @@ public class QuestionService {
 
         paginationDTO.setPagination(totalPage, page);
         Integer offset = size * (page - 1);
-        QuestionExample questionExample = new QuestionExample();
-        questionExample.setOrderByClause("gmt_create desc");
-        List<Question> questions = questionMapper.selectByExampleWithRowbounds(questionExample, new RowBounds(offset, size));
+        questionQueryDTO.setSize(size);
+        questionQueryDTO.setPage(offset);
+        List<Question> questions = questionExtMapper.selectBySearch(questionQueryDTO);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
 
         for (Question question : questions) {
